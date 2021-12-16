1c5f12c78e35d2c7e9f2adbcd56ea64ee0cf0baf
Hillary Topazian <80535782+htopazian@users.noreply.github.com>
2021-12-16 15:56:15 +0000

    adding section for Malaysia vars
    - removing old code notes
    - adding calcs for distance matrix from lat/long coords
    - adding jitter for participants with duplicate dates of infection

--- malaysia_pk.R
+++ malaysia_pk.R
@@ -6,6 +6,8 @@ library(VGAM)
 library(tensorflow)
 library(reticulate)
 library(scales)
+library(tidyverse)
+library(lubridate)
 
 # set working directory to directory where the function and data are stored
 setwd('C:/Users/htopazia/OneDrive - Imperial College London/Github/spatial_rcs')
@@ -34,7 +36,6 @@ reticulate::conda_version()
 # conda create -n r-reticulate tensorflow
 # conda env remove --name r-reticulate
 # conda activate r-reticulate
-# python
 # import tensorflow-probability
 
 # tell R which environment and package to use
@@ -42,7 +43,7 @@ use_condaenv("r-reticulate", required = TRUE)
 tfp <- reticulate::import("tensorflow_probability",convert=FALSE)
 tf_config() 
 
-# load simulated data
+# simulated data test ----------------------------------------------------------
 res <- read.csv('res.csv')
 
 n_imp <- 100 # imported cases are the first n_seed rows
@@ -88,7 +89,7 @@ sd_mid <- spatialnetrate(tp=tmat,
                          alpha = c(0.002, 0.001), # transmission rates
                          delta=c(0.01,0.001), # spatial parameters
                          SpatialKernel = "exponential", 
-                         epsilon = 1e-20) # high = lots of unobserved infection, low = little missing data
+                         epsilon = 1e-20) 
 
 # inspect results 
 
@@ -106,4 +107,75 @@ sd_mid[[4]]
 
 # AIC
 sd_mid[[5]]
+
+
+
+# Pk example -------------------------------------------------------------------
+MYSdata <- read_csv("MYSdata.csv")
+MYSdata <- MYSdata %>%          # fill in with variable names
+   mutate(case_number = case_number,
+          date_onset =  dmy(date_onset), # DD-MM-YYY
+          import =      import,          # if cases are imported (suspected from forest? no parent case in dataset)
+          longitude =   longitude,       # decimal degrees WGS84
+          latitude =    latitude) %>%    # decimal degrees WGS84
+
+   mutate(date = as.numeric(difftime(date_onset, dmy('01/01/2000'), unit='days')), # days since 2000
+          date = ifelse(duplicated(date), jitter(date, 1), date)) # if date is a duplicate, jitter point
+
+MYSdata <- st_as_sf(MYSdata, coords = c('longitude', 'latitude'), crs = 4326) # WGS84
+
+
+# make time and distance matrices
+dd <- list(n = nrow(MYSdata), # n observations
+           I = MYSdata$import, # imported cases
+           t = MYSdata$date, # time of infection
+           d = MYSdata$geometry) # spatial point
+
+# empty matrices for time and distance for each pair of observations
+tmat <- dmat <- matrix(0, nrow=dd$n, ncol=dd$n) 
+      
+      # shift time difference minimum serial interval 
+      SI <- 15 # set serial interval = 15 days
+      
+      for (i in 1:dd$n) { # for infectees
+        for(j in 1:(dd$n)){ # for all cases/infectors
+          tmat[i,j]=dd$t[i]-dd$t[j]-SI # find difference between case times - serial interval
+          
+        }
+      }
+      
+      tmat[tmat<0]=0 # anything less than zero is in the past so set time difference to zero
+      tmat <- tmat[-which(dd$I==1),] # imported cases cannot be infectees, remove these
+      
+      # calculate distance in decimal degrees
+      dmat = st_distance(MYSdata, MYSdata, which="Euclidean", by_element=F) 
+      dmat <- dmat[-which(dd$I==1),] # imported cases cannot be infectees, remove these
+      
+      # RUN FUNCTION
+      sd_mid <- spatialnetrate(tp = tmat, 
+                               dp = dmat, 
+                               DataType = euclidian,
+                               fixed = "epsilon", # fixed variables
+                               alpha = c(0.002, 0.001), # transmission rates
+                               delta = c(0.01,0.001), # spatial parameters
+                               SpatialKernel = "exponential", 
+                               epsilon = 1e-20) # high = lots of unobserved infection, 
+                                                # low = little missing data
+      
+      # inspect results 
+      
+      # distribution of Rc
+      hist(sd_mid[[1]])
+      
+      # delta parameter
+      sd_mid[[2]]
+      
+      # alpha parameter 
+      hist(sd_mid[[3]])
+      
+      # epsilon
+      sd_mid[[4]]
+      
+      # AIC
+      sd_mid[[5]]
 
--- tutorial.R
+++ tutorial.R
@@ -6,37 +6,72 @@ library(VGAM)
 library(tensorflow)
 library(reticulate)
 library(scales)
+# anaconda prompt cheatsheet
+# https://docs.conda.io/projects/conda/en/4.6.0/_downloads/52a95608c49671267e40c689e0bc00ca/conda-cheatsheet.pdf
 
-#set working directory to directory where the function and simulated data are stored
-setwd( "INSERTPATH" )
+
+# set working directory to directory where the function and simulated data are stored
+setwd('C:/Users/htopazia/OneDrive - Imperial College London/Github/spatial_rcs')
 
 # source function to generate RC
 source("function_genRC.R")
 
 # Set up miniconda environment and download tensorflow and tensorflow probability (old versions for compatibility with code)
-# Note need to change paths 
+# Note: need to change paths 
 
-reticulate::install_miniconda(path = "../AppData/Local/r-miniconda/", update = TRUE, force = FALSE)
+# tensorflow::install_tensorflow(envname = 'r-reticulate',
+#                                extra_packages = c("tensorflow-probability", "scipy"))
 
-Sys.setenv(RETICULATE_PYTHON="../AppData/Local/r-miniconda/envs/tensorflow")
 tensorflow::install_tensorflow(version = "1.14",
-                               extra_packages = c("tensorflow-probability==0.7" "scipy==1.5.0")  ,
-                               conda_python_version = "3.6",
-                               envname              = "tensorflow")
-conda_list()
-use_condaenv("tensorflow", required = TRUE)
+                               envname = 'r-reticulate',
+                               extra_packages = c("tensorflow-probability==0.7", "scipy==1.5.0"),
+                               conda_python_version = "3.6")
+
+reticulate::use_python('r-reticulate')
+Sys.setenv(RETICULATE_PYTHON="C:/Users/htopazia/Anaconda3/envs/r-reticulate")
+options(reticulate.conda_binary = "C:/Users/htopazia/Anaconda3/envs/r-reticulate")
+
+reticulate::py_config()
+reticulate::conda_version()
+
+# already installed these with install_tensorflow above
+# reticulate::conda_install('r-reticulate', 'tensorflow')
+# reticulate::conda_install('r-reticulate', 'tensorflow-probability')
+# reticulate::conda_install('r-reticulate', 'scipy')
+
+# anaconda command prompt arguments:
+# conda create -n r-reticulate tensorflow
+# conda env remove --name r-reticulate
+# conda activate r-reticulate
+# python
+# import tensorflow-probability
+
+# reticulate::conda_install('r-reticulate', 'tensorflow==1.14')
+# reticulate::conda_install('r-reticulate', 'tensorflow-probability==0.7')
+# reticulate::conda_install('r-reticulate', 'scipy==1.5.0')
+
+# reticulate = a way to use python in R (import = library)
+use_condaenv("r-reticulate", required = TRUE)
 tfp <- reticulate::import("tensorflow_probability",convert=FALSE)
 tf_config() 
 
-# Load simulated data
+# tfp <- reticulate::import_from_path("tensorflow-probability",path = "C:/Users/htopazia/Anaconda3/pkgs/tensorflow-probability-0.7-py_3", convert=FALSE)
 
+# Load simulated data
 res<-read.csv('res.csv')
 
-# make matrices and reformat for function  
-dd<-list(n=nrow(res), I= c(rep(1,length.out = n_seed), rep(0, length.out = nrow(res)-n_seed)),t=res$inf_times, d=res$inf_dist)
-tmat<-dmat<-matrix(0,nrow=dd$n,ncol=dd$n)
+n_seed <- 100 # imported cases are the first n_seed rows
 
-# shift for 15 day minimum serial interval 
+# make matrices and reformat for function  
+dd <- list(n=nrow(res), # n observations
+           I= c(rep(1,length.out = n_seed), rep(0, length.out = nrow(res)-n_seed)), # imported cases
+           t=res$inf_times, # time of infection
+           d=res$inf_dist) # spatial coordinates
+
+  # empty matrices for time and distance for each pair of observations
+tmat <- dmat <- matrix(0, nrow=dd$n, ncol=dd$n) 
+
+  # shift for 15 day minimum serial interval 
 for (i in 1:dd$n) { # for infectees
   for(j in 1:(dd$n)){ # for all cases/infectors
     tmat[i,j]=dd$t[i]-dd$t[j]-15 #otherwise calculate time difference		
@@ -45,21 +80,29 @@ for (i in 1:dd$n) { # for infectees
 }
 
 
-tmat[tmat<0]=0 # anything less than zero is in the past to set time difference to zero
-tmat = tmat[-which(dd$I==1),] # imported cases cant be infectees and so remove these
+tmat[tmat<0]=0 # anything less than zero is in the past so set time difference to zero
+tmat <- tmat[-which(dd$I==1),] # imported cases cant be infectees and so remove these
 
 # calculate distance in meters
 for (i in 1:dd$n) { # for infectees
   for(j in 1:(dd$n)){ # for all cases/infectors
-    dmat[i,j]= abs(dd$d[i]-dd$d[j]) #otherwise calculate time difference		
+    dmat[i,j]= abs(dd$d[i]-dd$d[j]) # otherwise calculate spatial absolute difference		
     
   }
 }
 
-dmat<-dmat/1000 # convert to kilometers
-dmat = dmat[-which(dd$I==1),] # imported cases cant be infectees and so remove these
+dmat <- dmat/1000 # convert to kilometers
+dmat <- dmat[-which(dd$I==1),] # imported cases cant be infectees and so remove these
 
-sd_mid<-spatialnetrate(tp=tmat, dp= dmat, fixed = "epsilon", alpha = c(0.002, 0.001), delta=c(0.01,0.001), SpatialKernel = "exponential", epsilon = 1e-20)
+# RUN FUNCTION
+sd_mid <- spatialnetrate(tp=tmat, 
+                         dp= dmat, 
+                         DataType = euclidian,
+                         fixed = "epsilon", 
+                         alpha = c(0.002, 0.001), # transmission rates
+                         delta=c(0.01,0.001), # spatial parameters
+                         SpatialKernel = "exponential", 
+                         epsilon = 1e-20) # high = lots of unobserved infection, low = little missing data
 
 # inspect results 
 
@@ -72,8 +115,9 @@ sd_mid[[2]]
 # alpha parameter 
 hist(sd_mid[[3]])
 
-#epsilon
+# epsilon
 sd_mid[[4]]
+
 # AIC
 sd_mid[[5]]
 
